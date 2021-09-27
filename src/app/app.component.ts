357ffed0167e33abefe07dc2e75b2cdb0647d364
tleveque <thierry.leveque@alithya.com>
2021-09-27 10:46:17 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { SelectItem } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   sales: any[];
